2c20eba66748976dfc32942c640ff1e553713c30
Aymane El Maini <aymane.elmaini@progressoft.com>
2026-01-08 21:01:43 +0100

    feat: initial project structure: added baseline headers, Makefile
    - added file system headers
    - added coding guidelines
    - added manifest for specs handling
    - added command line parsing
    - added header file for each command

---
+++ src/include/command.h
@@ -0,0 +1,18 @@
+#ifndef GENC_COMMAND_H
+#define GENC_COMMAND_H
+
+typedef enum
+{
+    COMMAND_TYPE_BUILD,
+    COMMAND_TYPE_INIT,
+    COMMAND_TYPE_NEW
+} CommandType;
+
+typedef struct
+{
+
+} CommandResult;
+
+typedef CommandResult (*CommandFunction)();  // this params could be update cuz we don't know how this will be
+
+#endif  // !GENC_COMMAND_H
---
+++ src/include/manifest_parser.h
@@ -0,0 +1,8 @@
+#ifndef GENC_MANIFEST_PARSER_H
+#define GENC_MANIFEST_PARSER_H
+
+#include "manifest.h"
+
+Manifest parse(const char* file_content);
+
+#endif  // !GENC_MANIFEST_PARSER_H
