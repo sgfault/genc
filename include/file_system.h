fae0939b817a9e0fea8198cbffcd69f92e3156c4
Sidatii <sidatnouhi@gmail.com>
2026-01-11 22:53:15 +0100

    addes two file system operations and header file for it.

--- include/file_system.h
+++ include/file_system.h
@@ -1,17 +1,20 @@
 #ifndef GENC_FILE_SYSTEM_H
 #define GENC_FILE_SYSTEM_H
 
+#include "../include/errors.h"
 // panic("not implemented yet, go use linux");
 
-void read_file(const char* path);
+void read_file(ErrorCollector errors, const char* path);
 
 // just create file if does not exist, if exists ignore it
-void create_file(const char* path);
+void create_file(ErrorCollector errors, const char* path);
 
 // write to a file, create it if does not exist then write to it
-void write_to_file(const char* path, const char* content);
+void write_to_file(ErrorCollector errors, const char* path, const char* content);
 
 // walk directories recusive...
-void walk_dir();
+void walk_dir(ErrorCollector errors, char* path);
+
+void create_dir(ErrorCollector errors, const char* name);
 
 #endif  // !GENC_FILE_SYSTEM_H
---
+++ src/file_system.c
@@ -0,0 +1,50 @@
+#include "../include/file_system.h"
+#include "../include/errors.h"
+#include <errno.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+void create_dir(ErrorCollector errors, const char* path)
+{
+
+    if (mkdir(path, 0755) && errno != EEXIST)
+    {
+        // fprintf(stderr, "Failed to create directory %s: %s\n", path, strerror(errno));
+        add_error(&errors, ERROR_TYPE_FILESYSTEM, &(FileSystemErrorType){FS_ERROR_CANNOT_CREATE_DIR}, "Failed to create directory", path);
+        report_and_exit(&errors);
+    }
+}
+
+void write_to_file(ErrorCollector errors, const char* path, const char* content)
+{
+    FILE* f = fopen(path, "w");
+    if (!f)
+    {
+        add_error(&errors, ERROR_TYPE_FILESYSTEM, &(FileSystemErrorType){FS_ERROR_CANNOT_OPEN_FILE}, "Failed to open file", path);
+        report_and_exit(&errors);
+    }
+    if (fputs(content, f) != 0)
+    {
+        add_error(&errors, ERROR_TYPE_FILESYSTEM, &(FileSystemErrorType){FS_ERROR_CANNOT_WRITE_TO_FILE}, "Failed to write to file", path);
+        report_and_exit(&errors);
+    }
+    fclose(f);
+}
+
+void read_file(ErrorCollector errors, const char* path)
+{
+    FILE* fd = fopen(path, "r");
+    if (!fd)
+    {
+        add_error(&errors, ERROR_TYPE_FILESYSTEM, &(FileSystemErrorType){FS_ERROR_CANNOT_READ_FILE}, "Failed to read file", path);
+        report_and_exit(&errors);
+    }
+}
+
+// just create file if does not exist, if exists ignore it
+void create_file(ErrorCollector errors, const char* path)
+{
+}
+
+// walk directories recusively...
+void walk_dir(ErrorCollector errors, char* path);
