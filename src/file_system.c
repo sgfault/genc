c7033c51317a31070d1ce29f151cbb8e783c425b
Sidatii <sidatnouhi@gmail.com>
2026-01-12 02:00:25 +0100

    changed variable name for better readability.

--- src/file_system.c
+++ src/file_system.c
@@ -7,32 +7,59 @@
 #include <string.h>
 #include <sys/stat.h>
 
+void create_dir(ErrorCollector* errors, const char* path)
+{
+
+    if (mkdir(path, 0755) && errno != EEXIST)
+    {
+        add_error(errors, FS_ERR_CANNOT_CREATE_DIR, "Failed to create directory", path);
+        report_and_exit(errors);
+    }
+}
+
+void write_to_file(ErrorCollector* errors, const char* path, const char* content)
+{
+    FILE* file_descriptor = fopen(path, "w");
+    if (!file_descriptor)
+    {
+        add_error(errors, FS_ERR_CANNOT_OPEN_FILE, "Failed to open file", path);
+        report_and_exit(errors);
+    }
+    if (fputs(content, file_descriptor) != 0)
+    {
+        add_error(errors, FS_ERR_CANNOT_WRITE_TO_FILE, "Failed to write to file", path);
+        fclose(file_descriptor);
+        report_and_exit(errors);
+    }
+    fclose(file_descriptor);
+}
+
 File read_file(ErrorCollector* errors, const char* path)
 {
-    FILE* f = fopen(path, "rb");
-    if (!f)
+    FILE* file_descriptor = fopen(path, "r");
+    if (!file_descriptor)
     {
         add_error(errors, FS_ERR_CANNOT_READ_FILE, "Failed to read file", path);
         report_and_exit(errors);
     }
 
-    fseek(f, 0, SEEK_END);
-    size_t size = ftell(f);
-    fseek(f, 0, SEEK_SET);
+    fseek(file_descriptor, 0, SEEK_END);
+    size_t size = ftell(file_descriptor);
+    fseek(file_descriptor, 0, SEEK_SET);
 
     char* buffer = malloc(size + 1);
     if (!buffer)
     {
-        fclose(f);
+        fclose(file_descriptor);
         add_error(errors, FS_ERR_CANNOT_READ_FILE, "Failed to allocate buffer.", "");
         report_and_exit(errors);
     }
 
-    size_t read = fread(buffer, 1, size, f);
+    size_t read = fread(buffer, 1, size, file_descriptor);
     buffer[read] = '\0';
     size_t length = read;
 
-    fclose(f);
+    fclose(file_descriptor);
     File result = {buffer, length};
     return result;
 }
