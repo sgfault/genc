053fe15273a7cc084477530e507343e3ac40c69b
Sidatii <sidatnouhi@gmail.com>
2026-01-12 02:55:59 +0100

    added documentation to command_new.h

--- include/command_new.h
+++ include/command_new.h
@@ -6,7 +6,15 @@
 
 CommandResult command_new();
 
-void init_project(ErrorCollector* errorCollector, const char* name);
+/**
+ * Initialize a project
+ * @param collector: error collector to add errors to if any
+ * @param type: type of error
+ * @param _template: template to use to initialize the project
+ * @param attribute: the value to use to replace the placeholder (could be name, author or any other attribute)
+ * @param placeholder: the placeholder used in the template eg: {{project_name}}
+ */
+void init_project(ErrorCollector* collector, const char* name);
 
 char* replace_placeholder(const char* _template, const char* attribute, const char* placeholder);
 
